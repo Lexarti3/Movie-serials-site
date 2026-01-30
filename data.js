95ecbaf3d4a8772360cd6bccff9203fc7d989116
Lexarti3 <Leharti@yandex.ru>
2026-01-30 16:32:03 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const movies = [
+  {
+    id: 1,
+    name: "Во все тяжкие",
+    overview: "История учителя химии, ставшего наркобароном.",
+    trailer: "https://www.youtube.com/embed/HhesaQXLuRY",
+    link: "https://www.kinopoisk.ru/series/404900/"
+  },
+  {
+    id: 2,
+    name: "Игра престолов",
+    overview: "Борьба за власть в мире Вестероса.",
+    trailer: "https://www.youtube.com/embed/KPLWWIOCOOQ",
+    link: "https://www.kinopoisk.ru/series/464963/"
+  }
+];
